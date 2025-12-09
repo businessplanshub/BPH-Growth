dbcebffc23d7ffb7bbb56f72f28a8d40aead276f
debire <damilareebire@gmail.com>
2025-12-09 15:24:28 +0100

    lock and cloudinary updated

--- backend/server.js
+++ backend/server.js
@@ -55,14 +55,6 @@ app.use((req, res, next) => {
   next()
 })
 
-const __filename = fileURLToPath(import.meta.url)
-const _dirname = path.dirname(_filename)
-
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
-})
-
 // Basic routes
 app.get('/', (req, res) => {
   res.json({
